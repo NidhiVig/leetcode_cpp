2119eb9493d123c56651c7f05fe7c72f0819a0d4
Nidhi <nidhivig27@gmail.com>
2022-01-30 18:44:19 +0530

    first commit

---
+++ validate_binary_tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& v){
+        if(root==NULL)
+         return;
+         inorder(root->left,v);
+         v.push_back(root->val);
+         inorder(root->right,v);
+    }
+    bool isValidBST(TreeNode* root) {
+       if(root == NULL)
+        return 1;
+        vector<int> v;
+        inorder(root,v);
+        
+        for(int i = 0;i<v.size()-1;++i){
+            if(v[i] >= v[i+1])
+            return 0;
+        }
+        return 1;
+    }
+};
