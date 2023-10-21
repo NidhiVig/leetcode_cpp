607724df88051adf62f5068a1ec3692d301c16de
Nidhi Vig <95757668+NidhiVig@users.noreply.github.com>
2023-10-21 06:36:58 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -10,37 +10,39 @@ class Solution
     int orangesRotting(vector<vector<int>>& grid) {
         // Code here
         int time = 0;
-        int n = grid.size(), m = grid[0].size();
+        int n = grid.size(), m = grid[0].size(), cnt = 0;
         vector<int> delr = {-1,0,1,0};
         vector<int> delc = {0,-1,0,1};
         queue<pair<int,int>> q;
-        // vector<vector<bool>> vis(n,vector<bool>(m,0));
         for(int i = 0;i<n;i++)
             for(int j = 0;j<m;j++)
-                if(grid[i][j] == 2) q.push({i,j});
+                if(grid[i][j] == 2)
+                    q.push({i,j});
+                else if(grid[i][j] == 1) cnt++;
         while(!q.empty()){
             int sz = q.size();
             bool flag = 0;
             for(int i = 0;i<sz;i++){
                 pair<int,int> p = q.front();
                 q.pop();
-                // bfs(p.first, p.second, grid, vis, n,m);
                 for(int ii = 0;ii<4;ii++){
                     int r = p.first+delr[ii], c = p.second+delc[ii];
                     if(r>=0 && r<n && c>=0 && c<m && grid[r][c] == 1){
                         q.push({r,c});
                         grid[r][c] = 2;
                         flag = 1;
+                        cnt--;
                     }
                 }
             }
             if(!flag) break;
             time++;
         }
-        for(int i = 0;i<n;i++)
-            for(int j = 0;j<m;j++)
-                if(grid[i][j] == 1) time = -1;
-        return time;
+        // for(int i = 0;i<n;i++)
+        //     for(int j = 0;j<m;j++)
+        //         if(grid[i][j] == 1) time = -1;
+        return cnt==0?time:-1;
+        // TC: O(n*m)
     }
 };
 
